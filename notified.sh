893d5d062e008668bff9721301c902052da3861c
Farasat Khan <farasatkahan@gmail.com>
2023-07-25 17:13:12 +0500

    added tee to display start and end of script

--- notified.sh
+++ notified.sh
@@ -9,8 +9,8 @@ input_file="$1"
 input_directory=$(dirname "$input_file")
 
 while true; do
-    echo "$(date) - Notified.sh script started" >> log.txt
+    echo "$(date) - Notified.sh script started" | tee -a log.txt
     amass enum -df "$input_file" -silent -config ./config/amass-config.ini | anew "$input_directory/subdomains.txt" | notify -silent -pc ./config/notify-config.yaml
-    echo "$(date) - Notified.sh script ended" >> log.txt
+    echo "$(date) - Notified.sh script ended" | tee -a log.txt
     sleep 86400
 done
